d110d2ab928bb938a768d328ab219bd108fd7c8d
emperorg7238-netizen <emperorg7238@gmail.com>
2026-08-18 11:34:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const orderForm = document.getElementById("orderForm");
+
+orderForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const product = document.getElementById("product").value;
+    const quantity = document.getElementById("quantity").value;
+    const delivery = document.getElementById("delivery").value;
+    const message = document.getElementById("message").value;
+
+    const whatsappNumber = "2348134375132";
+
+    const orderMessage =
+        `Hello Bhunny Poultry Farm 👋
+
+I would like to place an order.
+
+Name: ${name}
+Phone: ${phone}
+Product: ${product}
+Quantity: ${quantity}
+Option: ${delivery}
+
+Additional message:
+${message}`;
+
+    const whatsappURL =
+        `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(orderMessage)}`;
+
+    window.open(whatsappURL, "_blank");
+});
