f858a6fac62c5b4e3b256f46d261f63cd65c06c3
emperorg7238-netizen <emperorg7238@gmail.com>
2026-08-20 23:30:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -122,3 +122,27 @@ if (backToTop) {
     });
 
 }
+// ================================
+// BACK TO TOP BUTTON
+// ================================
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 500) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
+
+});
+
+backToTop.addEventListener("click", function () {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
